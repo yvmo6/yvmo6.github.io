4edb6d8367e01419ed5e490fceab77a6dddd4714
yvmo6 <liyuanda2@gmail.com>
2023-01-22 17:03:03 +0800

    Site updated: 2023-01-22 17:03:02

---
+++ js/weather.js
@@ -0,0 +1,21 @@
+WIDGET = {
+  "CONFIG": {
+    "modules": "01234",
+    "background": "5",
+    "tmpColor": "00FFFF",
+    "tmpSize": "16",
+    "cityColor": "00FFFF",
+    "citySize": "16",
+    "aqiColor": "00FFFF",
+    "aqiSize": "16",
+    "weatherIconSize": "24",
+    "alertIconSize": "18",
+    "padding": "10px 10px 10px 10px",
+    "shadow": "0",
+    "language": "zh",
+    "fixed": "false",
+    "vertical": "top",
+    "horizontal": "left",
+    "key": "f00dd2dbc56b469789c314cb70b2fe10"
+  }
+}
